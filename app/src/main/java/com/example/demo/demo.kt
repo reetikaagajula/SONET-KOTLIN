a98bd3e9de7a0129feca7033d6bf01f3942e5843
Reetikaa <reetikaagajula.2002@gmail.com>
2021-05-12 01:14:09 +0530

    _3rd_commit

--- app/src/main/java/com/example/demo/demo.kt
+++ app/src/main/java/com/example/demo/demo.kt
@@ -1,5 +1,13 @@
 package com.example.demo
 
 fun main() {
-    print("HELLO world")
+    var myInt = 25
+    var myFloat:Float =13.37F
+    var s="Android Kotlin"
+    var myDouble:Double =3.1415926535979
+    print(s+"\n")
+    print("$myFloat"+"\n")
+    print("$myDouble"+"\n")
+    print("$myInt"+"\n")
+
 }
