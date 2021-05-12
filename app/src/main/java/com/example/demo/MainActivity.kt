ba9a5ee28fcfc17f9fa6d74a5923ce0ebe3867e4
Reetikaa <reetikaagajula.2002@gmail.com>
2021-05-12 22:51:51 +0530

    _4th_commit

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var input_result = findViewById<EditText>(R.id.input_radius)
+        var btn = findViewById<Button>(R.id.btn)
+        var txt = findViewById<TextView>(R.id.txtView)
+        btn.setOnClickListener(View.OnClickListener {
+            var radius:Double = input_result.text.toString().toDouble()
+            val pi:Double = 3.141
+            var area = pi*radius*radius
+            txt.setText("Result : +$area")
+
+        })
     }
 }
