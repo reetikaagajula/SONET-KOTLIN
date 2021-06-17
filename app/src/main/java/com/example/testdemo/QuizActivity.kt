6f09c8503b71ad4859143d3657dad896468d2545
reetikaagajula <reetikaagajula.2002@gmail.com>
2021-06-17 20:50:13 +0530

    day2_commit

--- app/src/main/java/com/example/testdemo/QuizActivity.kt
+++ app/src/main/java/com/example/testdemo/QuizActivity.kt
@@ -2,10 +2,39 @@ package com.example.testdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class QuizActivity : AppCompatActivity() {
+
+    private var listOfQuestions: ArrayList<Questions>? = null
+    var currentPosition = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        var Utility = utility()
+        listOfQuestions = Utility.getQuestion()
+
+        setQuestion();
+    }
+
+    fun setQuestion(){
+        var questionObject = listOfQuestions!!.get(currentPosition-1)
+        var question = findViewById<TextView>(R.id.q_question)
+        question.text= questionObject.question
+
+        var optionOne = findViewById<TextView>(R.id.option_1)
+        optionOne.text= questionObject.optOne
+
+        var optionTwo = findViewById<TextView>(R.id.option_2)
+        optionTwo.text= questionObject.optTwo
+
+        var optionThree = findViewById<TextView>(R.id.option_3)
+        optionThree.text= questionObject.optThree
+
+        var optionFour = findViewById<TextView>(R.id.option_4)
+        optionFour.text= questionObject.optFour
+
+
     }
 }
